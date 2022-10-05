a91e0d9b275f34940c3613ea8e1c53830efae3e5
limbaba1120 <limkw1120ss@gmail.com>
2022-10-05 22:29:44 +0900

    application development second commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        //code
+        try {
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("HelloA");
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
